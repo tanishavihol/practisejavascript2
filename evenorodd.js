d805a617a949f969bfef91acee17189ec4df139c
tanishavihol <157605807+tanishavihol@users.noreply.github.com>
2024-07-09 20:19:57 +0530

    Add files via upload

---
+++ evenorodd.js
@@ -0,0 +1,6 @@
+function isEven(num) {
+    return num % 2 === 0;
+  }
+  console.log(isEven(4)); // Output: true
+  console.log(isEven(3)); // Output: false
+  
---
+++ largest.js
@@ -0,0 +1,5 @@
+function findLargest(arr) {
+    return Math.max(...arr);
+  }
+  console.log(findLargest([1, 2, 3, 4, 5])); // Output: 5
+  
